9d0b178ca458d883d84a344f5bcb95c565f1a6d2
Angel0973180707 <a0973180707@gmail.com>
2026-02-16 09:17:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+const SHEET_URL = "你的大陆服务器API地址";
 
+async function loadData(){
+  try{
+    const res = await fetch(SHEET_URL);
+    const data = await res.json();
+
+    document.getElementById("name").innerText = data.name;
+    document.getElementById("unit").innerText = data.unit;
+    document.getElementById("service").innerText = data.service;
+
+  }catch(e){
+    document.getElementById("name").innerText = "数据加载失败";
+  }
+}
+
+function copyLink(){
+  navigator.clipboard.writeText(window.location.href);
+  alert("链接已复制");
+}
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('sw.js');
+}
+
+loadData();
